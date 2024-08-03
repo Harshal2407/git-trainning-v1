bb0c953e9275c451eed8ddc8827d8ebc7a852aa3
Harshal2407 <harshaldahiwal@gmail.com>
2024-08-03 20:01:29 +0530

    Updated exl file from techteam1

--- exl-forloop.py
+++ exl-forloop.py
@@ -1,3 +1,5 @@
 print("first modification fir 1st commit")
 #-----------------------------------------------------
 print("Added this line of code from git terminal")
+#-----------------------------------------------------
+print("Added 1st line from techteam1 on GitHub")
